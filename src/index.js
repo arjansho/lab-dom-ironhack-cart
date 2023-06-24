84c6f6b78dfaf618efdc55d4b214424f95166d1d
Arjansho1~ <arjansho@amazon.de>
2023-06-24 12:19:28 +0200

    Solved lab

--- src/index.js
+++ src/index.js
@@ -1,42 +1,73 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  const price = Number(product.querySelector(".price span").innerText);
+  const quantity = Number(product.querySelector(".quantity input").value);
+  const subtotal = price * quantity;
+  product.querySelector(".subtotal span").innerText = subtotal.toFixed(2);
 
-  //... your code goes here
+  return subtotal;
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+  const testElements = document.getElementsByClassName('product');
+  let total = 0
+  for (let i = 0; i < testElements.length; i++) {
+      total += updateSubtotal(testElements[i])
+  }
+  let totalNumber = document.querySelector('#total-value span')
+  totalNumber.innerHTML = total
+  console.log(totalNumber)
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  const buttonHolder = target.parentElement;
+  const productToBeRemoved = buttonHolder.parentElement;
+  productToBeRemoved.parentElement.removeChild(productToBeRemoved);
+
+  
 }
 
-// ITERATION 5
+// ITERATION 5   I have tried to fix the following section but still I am not able to run it. It cannot add the new product. I am missing something.
+// Please at the review by your side can you help and explain what is the issue. Thank you.
 
-function createProduct() {
-  //... your code goes here
+function createProduct(createProduct) {
+  const newProductName = document.getElementById("new-product-name");
+  const newProductPrice = document.getElementById("new-product-price");
+
+  const productList = document.getElementById("product-list");
+  productList.innerHTML += `<tr class="create-product">
+  <td class="name">
+    <span>${newProductName.value}</span>
+  </td>
+  <td class="price">$<span>${newProductPrice.value}</span></td>
+  <td class="quantity">
+    <input type="number" value="0" min="0" placeholder="Quantity" />
+  </td>
+  <td class="subtotal">$<span>0</span></td>
+  <td class="action">
+    <button class="btn btn-remove">Remove</button>
+  </td>
+</tr>`;
+  newProductName.value = "";
+  newProductPrice.value = 0;
+  addRemoveEvent();
 }
 
-window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
-
-  //... your code goes here
+window.addEventListener("load", () => {
+  const calculatePricesBtn = document.getElementById("calculate");
+  calculatePricesBtn.addEventListener("click", calculateAll);
+  addRemoveEvent();
+  const addButton = document.getElementById("create");
+  addButton.onclick = createProduct;
 });
+
+function addRemoveEvent() {
+  const removeButtons = document.getElementsByClassName("btn-remove");
+  for (let i = 0; i < removeButtons.length; i++) {
+    removeButtons[i].onclick = removeProduct;
+  }
+}
